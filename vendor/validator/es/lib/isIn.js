a05ce4e8c7acdd05bd72e5035448abea2d69d735
EmreUYGUNX <emreemr12@gmail.com>
2025-10-14 01:09:57 +0300

    fix(audit): vendor patched validator to unblock pnpm audit

---
+++ vendor/validator/es/lib/isIn.js
@@ -0,0 +1,43 @@
+import assertString from "./util/assertString";
+import toString from "./util/toString";
+
+function _typeof(o) {
+  "@babel/helpers - typeof";
+  return (
+    (_typeof =
+      "function" == typeof Symbol && "symbol" == typeof Symbol.iterator
+        ? function (o) {
+            return typeof o;
+          }
+        : function (o) {
+            return o &&
+              "function" == typeof Symbol &&
+              o.constructor === Symbol &&
+              o !== Symbol.prototype
+              ? "symbol"
+              : typeof o;
+          }),
+    _typeof(o)
+  );
+}
+
+export default function isIn(str, options) {
+  assertString(str);
+  var i;
+  if (Object.prototype.toString.call(options) === "[object Array]") {
+    var array = [];
+    for (i in options) {
+      // https://github.com/gotwarlost/istanbul/blob/master/ignoring-code-for-coverage.md#ignoring-code-for-coverage-purposes
+      // istanbul ignore else
+      if ({}.hasOwnProperty.call(options, i)) {
+        array[i] = toString(options[i]);
+      }
+    }
+    return array.indexOf(str) >= 0;
+  } else if (_typeof(options) === "object") {
+    return options.hasOwnProperty(str);
+  } else if (options && typeof options.indexOf === "function") {
+    return options.indexOf(str) >= 0;
+  }
+  return false;
+}
---
+++ vendor/validator/es/lib/isNumeric.js
@@ -0,0 +1,13 @@
+import { decimal } from "./alpha";
+import assertString from "./util/assertString";
+
+var numericNoSymbols = /^[0-9]+$/;
+export default function isNumeric(str, options) {
+  assertString(str);
+  if (options && options.no_symbols) {
+    return numericNoSymbols.test(str);
+  }
+  return new RegExp(
+    "^[+-]?([0-9]*[".concat((options || {}).locale ? decimal[options.locale] : ".", "])?[0-9]+$"),
+  ).test(str);
+}
